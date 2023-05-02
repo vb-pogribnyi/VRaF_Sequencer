7b7c1fcec6847278da51c6fb8491aaf6cc6d8423
Vitalii Pogribnyi <vb.pogribnyi@gmail.com>
2023-05-02 07:53:50 +0300

    Comments

--- main.cpp
+++ main.cpp
@@ -11,33 +11,35 @@ int main() {
 	GLFWwindow* window;
 	int width = 900;
 	int height = 400;
-
 	if (!glfwInit()) std::cout << "GLFW init failed" << std::endl;
 	window = glfwCreateWindow(width, height, "GLFW window", 0, 0);
     glfwMakeContextCurrent(window);
 
-    // Setup Dear ImGui context
     IMGUI_CHECKVERSION();
     ImGui::CreateContext();
     ImGuiIO &io = ImGui::GetIO();
-    // Setup Platform/Renderer bindings
     ImGui_ImplGlfw_InitForOpenGL(window, true);
     if (!ImGui_ImplOpenGL3_Init("#version 330")) std::cout << "ImGui OpenGL init failed" << std::endl;
 
+	// The values to be manipulated
     float temp_val = 0;
     glm::vec2 temp_val2 (0, 0);
-    VRaF::Sequencer sequencer;
+	// Crate Sequencer object. Can also accept fps in its constructor
+	// So to make it run 120 frames per second, it's sufficient to call
+	// VRaF::Sequencer sequencer(120); 
+    VRaF::Sequencer sequencer;  // Defalut fps is 30
 	sequencer.track("Test value", &temp_val);
 	sequencer.track("Test val2", &temp_val2);
     
 	while (!glfwWindowShouldClose(window)) {
 		glfwPollEvents();
         glClear(GL_COLOR_BUFFER_BIT);
-		float now = glfwGetTime();
 
 		ImGui_ImplOpenGL3_NewFrame();
 		ImGui_ImplGlfw_NewFrame();
 		ImGui::NewFrame();
+
+		// User Interface setup
 		ImGui::Begin("Sequencer Options");
 		ImGui::SliderFloat("Animation value", (float*)&temp_val, 0, 10);
 		ImGui::SliderFloat2("Animation value", (float*)&temp_val2, 0, 10);
@@ -45,10 +47,12 @@ int main() {
 		ImGui::Begin("Sequencer", 0, 16);
 		sequencer.draw();
 		ImGui::End();
-		sequencer.update(now);
+		// We want to let the sequencer know the current time,
+		// so that it's able to run properly
+		sequencer.update((float)glfwGetTime());
+
 		ImGui::Render();
 		ImGui_ImplOpenGL3_RenderDrawData(ImGui::GetDrawData());
-		//std::cout << "Hello" << std::endl;
 		glfwSwapBuffers(window);
 	}
 	ImGui_ImplOpenGL3_Shutdown();
